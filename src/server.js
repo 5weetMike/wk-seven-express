e85b2b97fe39b6b267c09c19f7cacb5f33c59bba
Michael Hampson <mdavidhampson@gmail.com>
2024-09-12 11:47:10 +0100

    refactor db

--- src/server.js
+++ src/server.js
@@ -1,38 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+//new code below added
+const connection = require("./db/connection");
+const  Book = require("./books/model");
+
+
 const app = express();
 app.use(express.json());
 
-console.log(process.env.MY_WORD);
-
-//DB connection
-
-const connection = async () => {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("DB is working")
-}
-
 connection();
 
-//Book model
 
-const bookSchema = new mongoose.Schema({
-    title:{
-        type: String,
-        required: true,
-        unique: true,        
-    },
-    author:{
-        type: String,
-        required: true,
-    },
-    genre:{
-        type: String,
-    },
-});
 
-const Book = mongoose.model("book", bookSchema);
+
 
 //Routes
 
